30bb209e90254c7208100a07b4162c51b7b93804
Ernest Epistola <ernestepistola@gmail.com>
2025-04-27 10:36:40 +0800

    Removed client app.

--- client/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",  // This ensures Tailwind purges unused styles in production
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
